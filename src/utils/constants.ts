632f8b6f5231f709d7c90ada7c09dcb13f7b8a03
Jaehoon Pyon <pjaehoon@vt.edu>
2026-01-20 09:46:13 -0500

    Video Update

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,9 +2,16 @@
 import { Video } from '@/types';
 
 export const MOCK_VIDEOS: Video[] = [
-  { id: "a", title: "Lamp", url: "/videos/a.MOV", thumbnail: "/thumbs/a.png" },
-  { id: "b", title: "Bowl", url: "/videos/b.MOV", thumbnail: "/thumbs/b.png" },
-  { id: "c", title: "Dragon", url: "/videos/c.MOV", thumbnail: "/thumbs/c.png" },
+  { id: "1", title: "Cafe Chaos", url: "/videos/1_Cafe Chaos 5 minute video.mp4", thumbnail: "" },
+  { id: "2", title: "Zadies Shell Shuffle", url: "/videos/2_Zadies Shell Shuffle 5 minute video.mp4", thumbnail: "" },
+  { id: "3", title: "One of These Goats", url: "/videos/3_OneOfTheseGoats_5Mins.mp4", thumbnail: "" },
+  { id: "4", title: "Buried Treasure", url: "/videos/7_BurriedTreasure_5mins.mp4", thumbnail: "" },
+  { id: "5", title: "Building Bridges", url: "/videos/BuildingBridges_5mins.mp4", thumbnail: "" },
+  { id: "6", title: "Bunny Hunt", url: "/videos/BunnyHunt_5mins.mp4", thumbnail: "" },
+  { id: "7", title: "Design Time", url: "/videos/Design Time.mp4", thumbnail: "" },
+  { id: "8", title: "Lemonade Problem", url: "/videos/Lemonade Problem - 5 mins.mp4", thumbnail: "" },
+  { id: "9", title: "Pokey Plant", url: "/videos/PokeyPlant_5mins.mp4", thumbnail: "" },
+  { id: "10", title: "Rocket Ride", url: "/videos/RocketRide_5mins.mp4", thumbnail: "" },
 ];
 
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api';
