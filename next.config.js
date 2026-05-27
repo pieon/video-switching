d1550573c1767a4d721ae046cb58e87e68067e8e
Jaehoon Pyon <pjaehoon@vt.edu>
2026-05-26 23:03:50 -0400

    Error fix and new calibration page

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/video',           // ← add
-  assetPrefix: '/video',        // ← add
   // swcMinify is now enabled by default in Next.js 16+
 
   // Uncomment for static export deployment
